c3ac2361104e84c472c9b3f31527c58723069d8c
uxmar <gabrielaquilarque97@gmail.com>
2013-09-15 20:55:13 -0430

    Agregado todo el algoritmo para linea 2.

--- train_ccs.py
+++ train_ccs.py
@@ -23,9 +23,8 @@ class train_ccs(object):
           path=recursive_dfs(graph, node, path)
       return path
 
-    def get_trans(self,list_path):
+    def get_trans(self,list_path,class_mdata):
         list_tu_trans=[]
-        class_mdata = master_data()
         qty_trans=0
         
         for path in list_path:
@@ -35,14 +34,87 @@ class train_ccs(object):
                     qty_trans = qty_trans + 1
         
         return list_tu_trans, qty_trans
+
+    def line_2(self,list_sta_partial,class_mdata,path):
         
+        dict_connec = class_mdata.connec
+        
+        #Zona Rental - Las Adjuntas / Zona Rental - Zoologico
+        if list_sta_partial[0] in class_mdata.direction['4'].keys() and \
+        path[len(path)-1] in class_mdata.direction['99'].keys():
+            if class_mdata.connec.get('Teatros',) and \
+            class_mdata.connec['Teatros'].get('Capuchinos',):
+                class_mdata.connec['Teatros'].pop('Capuchinos')
+            if class_mdata.connec.get('Mamera',) and \
+            class_mdata.connec['Mamera'].get('Ruiz Pineda',):
+                class_mdata.connec['Mamera'].pop('Ruiz Pineda')
+        
+        #Silencio - Las Adjuntas / Silencio - Zoologico
+        if list_sta_partial[0] in class_mdata.direction['2'].keys() and \
+        path[len(path)-1] in class_mdata.direction['99'].keys():
+            if class_mdata.connec.get('Mamera',) and \
+            class_mdata.connec['Mamera'].get('Caricuao',):
+                class_mdata.connec['Mamera'].pop('Caricuao')
+        
+        #Zoologico - Zona Rental
+        if list_sta_partial[0] in class_mdata.direction['20'].keys() and \
+        path[len(path)-1] in class_mdata.direction['4'].keys():
+            if class_mdata.connec.get('Capuchinos',) and \
+            class_mdata.connec['Capuchinos'].get('Teatros',):
+                class_mdata.connec['Capuchinos'].pop('Teatros')
+            if class_mdata.connec.get('Ruiz Pineda',) and \
+            class_mdata.connec['Ruiz Pineda'].get('Mamera',):
+                class_mdata.connec['Ruiz Pineda'].pop('Mamera')
+                
+        #Las Adjuntas - Zona Rental
+        if list_sta_partial[0] in class_mdata.direction['21'].keys() and \
+        path[len(path)-1] in class_mdata.direction['4'].keys():
+            if class_mdata.connec.get('Capuchinos',) and \
+            class_mdata.connec['Capuchinos'].get('Teatros',):
+                class_mdata.connec['Capuchinos'].pop('Teatros')
+            if class_mdata.connec.get('Ruiz Pineda',) and \
+            class_mdata.connec['Ruiz Pineda'].get('Mamera',):
+                class_mdata.connec['Ruiz Pineda'].pop('Mamera')
+        
+        #Zoologico - Silencio
+        if list_sta_partial[0] in class_mdata.direction['20'].keys() and \
+        path[len(path)-1] in class_mdata.direction['2'].keys():
+            if class_mdata.connec.get('Caricuao',) and \
+            class_mdata.connec['Caricuao'].get('Mamera',):
+                class_mdata.connec['Caricuao'].pop('Mamera')
+        
+        #Las Adjuntas - Silencio
+        if list_sta_partial[0] in class_mdata.direction['21'].keys() and \
+        path[len(path)-1] in class_mdata.direction['2'].keys():
+            if class_mdata.connec.get('Caricuao',) and \
+            class_mdata.connec['Caricuao'].get('Mamera',):
+                class_mdata.connec['Caricuao'].pop('Mamera')
+
+    def get_direction(self,list_sta_partial,class_mdata):
+        
+        valor_dir=0
+        direction=''
+        
+        pos_stab=len(list_sta_partial)-1
+        valor_dir = class_mdata.direction[class_mdata.line \
+        [list_sta_partial[0]]] \
+        [list_sta_partial[0]]-class_mdata.direction \
+        [class_mdata.line[list_sta_partial[pos_stab]]] \
+        [list_sta_partial[pos_stab]]
+    
+        if valor_dir >=1:
+            direction = class_mdata.direction[class_mdata.line[list_sta_partial[0]]]['direction'][1]
+        elif valor_dir < 0:
+            direction = class_mdata.direction[class_mdata.line[list_sta_partial[0]]]['direction'][0]
+            
+        return direction, pos_stab
+
     def get_options(self,start,end):
         class_mdata = master_data()
         list_path = self.find_all_paths(class_mdata.graph,start,end)
         list_sta_trans= []
         list_sta_partial=[]
         llist_sta_partial=[]
-        direction=''
 
         print start, end
 
@@ -66,35 +138,19 @@ class train_ccs(object):
             
             for i,j in zip(path,path[1::]):
                 list_sta_partial.append(i)
-                valor_dir=0
                 #~ if class_mdata.connection.get(i,) and j==class_mdata.connection[i]: 
                     #~ qty_trans = qty_trans + 1  #se puede retornar de la funcion get_trans
 
-
-                if list_sta_partial[0] in class_mdata.direction['2'].keys() and path[len(path)-1] in ['Zoologico','Caricuao','Las Adjuntas','Ruiz Pineda']:
-                    if class_mdata.connec.get('Mamera',) and class_mdata.connec['Mamera'].get('Caricuao',):
-                        class_mdata.connec['Mamera'].pop('Caricuao')
-                
-
-
-                list_trans, qty_trans = self.get_trans(list_path)
+                self.line_2(list_sta_partial,class_mdata,path)
+                list_trans, qty_trans = self.get_trans(list_path,class_mdata)
                 
                 if (i,j) in list_trans:
-                    pos_stab=len(list_sta_partial)-1
-                    valor_dir = class_mdata.direction[class_mdata.line \
-                    [list_sta_partial[0]]] \
-                    [list_sta_partial[0]]-class_mdata.direction \
-                    [class_mdata.line[list_sta_partial[pos_stab]]] \
-                    [list_sta_partial[pos_stab]]
+                    
+                    direction, pos_stab = self.get_direction(list_sta_partial,class_mdata)
                     
                     print 'Desde', list_sta_partial[0]
                     print 'Hasta', list_sta_partial[pos_stab]
                     print 'valor_dir',valor_dir
-                    
-                    if valor_dir >=1:
-                        direction = class_mdata.direction[class_mdata.line[list_sta_partial[0]]]['direction'][1]
-                    elif valor_dir < 0:
-                        direction = class_mdata.direction[class_mdata.line[list_sta_partial[0]]]['direction'][0]
                     
                     if class_mdata.line[j] in ['21','20']:
                         line='2'
@@ -102,6 +158,7 @@ class train_ccs(object):
                         line=class_mdata.line[j]
                         
                     list_path_end.append('Ingrese al tren con direccion ' + direction)
+                    print 'list_sta_partial', list_sta_partial
 
                     if class_mdata.connec[i][j]:
                         list_path_end.append('Continue ' + str(len(list_sta_partial)) + ' estaciones en esta linea hasta la estacion ' + list_sta_partial[pos_stab])
@@ -114,17 +171,7 @@ class train_ccs(object):
                     list_sta_partial=[]
 
             list_sta_partial.append(j)
-            pos_stab=len(list_sta_partial)-1
-            valor_dir = class_mdata.direction[class_mdata.line \
-            [list_sta_partial[0]]] \
-            [list_sta_partial[0]]-class_mdata.direction \
-            [class_mdata.line[list_sta_partial[pos_stab]]] \
-            [list_sta_partial[pos_stab]]
-        
-            if valor_dir >=1:
-                direction = class_mdata.direction[class_mdata.line[list_sta_partial[0]]]['direction'][1]
-            elif valor_dir < 0:
-                direction = class_mdata.direction[class_mdata.line[list_sta_partial[0]]]['direction'][0]
+            direction, pos_stab = self.get_direction(list_sta_partial,class_mdata)
             
             list_path_end.append('Ingrese al tren con direccion ' + direction)
             llist_sta_partial.append(list_sta_partial)
@@ -161,7 +208,7 @@ def main():
     class_master_data = master_data()
     class_train_ccs = train_ccs()
     
-    print class_train_ccs.get_options('Agua Salud','Zoologico')
+    print class_train_ccs.get_options('Sabana Grande','Zoologico')
     #~ print class_train_ccs.transfer_qty()
     
     #~ class_train_ccs.get_list_path_decription()
